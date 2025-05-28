bac63ef259f82686add8e09456f9852d69a07c01
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-05-28 16:57:18 +0700

    Add BusinessObjectsLayer

---
+++ NguyenKhanhMinh_SE1729_A01_BE/BusinessObjectsLayer/Models/AdminAccountSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjectsLayer.Models
+{
+    public class AdminAccountSettings
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+    }
+}
